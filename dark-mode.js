d26aa6d91bc30e54212c284f79335758262b0864
Paulo <paulorzzbj@gmail.com>
2026-03-13 13:17:09 +0100

    some fetch almost darkmode button

---
+++ dark-mode.js
@@ -0,0 +1,32 @@
+const root = document.documentElement;
+const switchElm = document.querySelector(".light-switch");
+const savedTheme = readFromLocalStorage("isDarkMode");
+const systemPrefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+init();
+function init() {
+    let isDark;
+    if (savedTheme !== null && savedTheme !== undefined) {
+        isDark = savedTheme;
+    } else {
+        isDark = systemPrefersDark;
+    }
+    switchElm.checked = isDark;
+    root.dataset.dark = isDark;
+}
+switchElm.addEventListener("change", () => {
+    const isDark = switchElm.checked;
+    root.setAttribute("data-dark", isDark);
+    saveToLocalStorage("isDarkMode", isDark);
+});
+function saveToLocalStorage(key, value) {
+    localStorage.setItem(key, JSON.stringify(value))
+    return "Data was saved with the key " + key
+}
+function readFromLocalStorage(key) {
+    return JSON.parse(localStorage.getItem(key))
+}
+function deleteFromLocalStorage(key) {
+    localStorage.removeItem(key)
+    return "The element with key " + key + " was deleted.";
+}
---
+++ my-movies.js
@@ -0,0 +1,91 @@
+const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NDc0OTdkMmVhMTg4ODgzMWRlOTgzZTY3MjFlNzcxOSIsIm5iZiI6MTc3MzMwNzU1MC42MDQsInN1YiI6IjY5YjI4NjllNDYwNGZhYzNjNzRlZmJhNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Vmf_pWhXeE465Jzmk3i96jPQL5nJtnNwIQCXzdtf1vc'
+    }
+};
+const apiKey = "547497d2ea1888831de983e6721e7719";
+const baseImgPath = "http://image.tmdb.org/t/p";
+const nowShowingUrl = "https://api.themoviedb.org/3/movie/now_playing";
+const popularUrl = "https://api.themoviedb.org/3/movie/popular";
+const nowBoxDom = document.querySelector("#now-showing-movie-box");
+const popularBoxDom = document.querySelector("#popular-movie-box");
+
+fetch('https://api.themoviedb.org/3/configuration', options)
+    .then(res => res.json())
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
+fetch('https://api.themoviedb.org/3/movie/now_playing', options)
+    .then(res => res.json())
+    .then((data) => {
+        displayNowShowing(data)
+        console.log(data)
+    })
+fetch('https://api.themoviedb.org/3/movie/popular', options)
+    .then(res => res.json())
+    .then((data) => {
+        displayPopular(data)
+        console.log(data)
+    })
+/* fetch('https://api.themoviedb.org/3/genre/movie/list?language=en', options)
+  .then(res => res.json())
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
+*//*
+async function fetchPopular() {
+    const popularMovieFetch = await fetch('https://api.themoviedb.org/3/movie/popular', options)
+    const genreFetch = await fetch('https://api.themoviedb.org/3/genre/movie/list?language=en', options)
+    .then((data) => {
+        displayPopular(data)
+        console.log(data)
+    })
+} */
+
+function displayNowShowing(data) {
+    const results = data.results
+    const nowString = results.map((result) => {
+        const {id, title, poster_path, vote_average} = result
+        return /*html*/ `
+        <li>
+            <article class="now-movie-card">
+                <img src="${baseImgPath}/w185${poster_path}" alt="${title} poster">
+                <h3>${title}</h3>
+                <p class="rating">
+                    <span class="star">
+                        <svg></svg>
+                    </span>
+                    ${vote_average}/10 IMDb
+                </p>
+            </article>
+            <a href="detail.html?id=${id}&url=${nowShowingUrl}&name=${title}"></a>
+        </li>`
+    }).join("")
+    nowBoxDom.insertAdjacentHTML("beforeend", nowString)
+}
+
+function displayPopular(data) {
+    const results = data.results
+    const popularString = results.map((result) => {
+        const {id, genre_ids, title, vote_average, poster_path} = result
+        return /*html*/ `
+        <li>
+            <article class="popular-movie-card">
+                <img src="${baseImgPath}/w92${poster_path}" alt="${title} poster">
+                <h3>${title}</h3>
+                <p class="rating">
+                    <span class="star">
+                        <svg></svg>
+                    </span>
+                    ${vote_average}/10 IMDb
+                </p>
+                <ul class="genre-list">
+                    <li>${genre_ids}</li>
+                </ul>
+                <p class="length"></p>
+            </article>
+            <a href="detail.html?id=${id}&url=${popularUrl}&name=${title}"></a>
+        </li>`
+    }).join("")
+    popularBoxDom.insertAdjacentHTML("beforeend", popularString)
+}
